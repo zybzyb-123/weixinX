5a60d33835c1cb2380d72dec799a1f6e4c3e817b
zyb_01_01 <1595346022@qq,com>
2021-11-03 16:56:21 +0800

    Initial commit

---
+++ weixin-web/weixin-eureka/src/test/java/com/zyb/weixineureka/WeixinEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyb.weixineureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeixinEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
